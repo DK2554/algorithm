9df0cb28f2381f3c234b6747868bf5feb4febfae
DK2554 <ldu0215@gmail.com>
2020-12-12 14:12:23 +0900

    제곱근

---
+++ src/com/algorithm/SqrtPow.java
@@ -0,0 +1,11 @@
+package com.algorithm;
+
+public class SqrtPow {
+	public static void main(String[] args) {
+		double x=Math.sqrt(-5);
+		System.out.println("X의제곱근:"+x);
+		int pow=(int)Math.pow(5, 2);
+		System.out.println("pow는제곱을 계산하는 Math의 메소드:"+pow);
+	}
+
+}
